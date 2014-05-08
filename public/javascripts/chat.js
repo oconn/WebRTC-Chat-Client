12bc30e77ab93a310a43662a49081274d8c7a0f1
Matthew O'Connell <matthew.j.oconnell1@gmail.com>
2014-05-08 18:20:35 -0400

    add redis

--- public/javascripts/chat.js
+++ public/javascripts/chat.js
@@ -1,5 +1,24 @@
-var socket = io.connect('http://localhost');
-socket.on('news', function (data) {
-  console.log(data);
-  socket.emit('my other event', { my: 'data' });
+$(document).ready(function(){
+  var socket = io.connect('http://localhost:3000');
+  socket.on('welcome', function (data) {
+    // alert(data.msg);
+    // socket.emit('my other event', { my: 'data' });
+  });
+
+  socket.on("add chatter", function(chatter){
+    $("#chat-list").append('<li>' + chatter + '</li>');
+  });
+
+  $(".loginForm").on('submit', function(e){
+    e.preventDefault();
+    
+    var name = $.trim($("#userName").val());
+    
+    if(name.length < 1){
+      $("#userName").css("border", "1px solid red");
+    } else {
+      socket.emit("login", {name: name});
+      $(this).slideUp();
+    }
+  });
 });
