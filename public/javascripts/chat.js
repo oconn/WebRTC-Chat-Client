a9c3fd141d0eaa3795eee9e02632224c4fca7df1
Matthew O'Connell <matthew.j.oconnell1@gmail.com>
2014-05-08 19:08:09 -0400

    allow for messaging between clients

--- public/javascripts/chat.js
+++ public/javascripts/chat.js
@@ -1,24 +1,41 @@
-$(document).ready(function(){
-  var socket = io.connect('http://localhost:3000');
-  socket.on('welcome', function (data) {
-    // alert(data.msg);
-    // socket.emit('my other event', { my: 'data' });
-  });
+var socket = io.connect('http://localhost:3000');
 
-  socket.on("add chatter", function(chatter){
-    $("#chat-list").append('<li>' + chatter + '</li>');
-  });
-
-  $(".loginForm").on('submit', function(e){
-    e.preventDefault();
-    
-    var name = $.trim($("#userName").val());
-    
-    if(name.length < 1){
-      $("#userName").css("border", "1px solid red");
-    } else {
-      socket.emit("login", {name: name});
-      $(this).slideUp();
-    }
-  });
+$(".loginForm").on('submit', function(e){
+  e.preventDefault();
+  
+  var name = $.trim($("#userName").val());
+  
+  if(name.length < 1){
+    $("#userName").css("border", "1px solid red");
+  } else {
+    socket.emit("login", {name: name});
+    $(this).slideUp();
+    listenForChatter();
+  }
 });
+
+function listenForChatter(){
+  socket.on("add chatter", function(chatter){
+    $("#chat-list ul").append('<li data-name="' + chatter + '">' + chatter + '</li>');
+  });
+
+  socket.on("remove chatter", function(chatter){
+    $("#chat-list ul li[data-name=" + chatter + "]").remove();
+  });
+
+  socket.on("chat", function(msg){
+    $("#messages").append("\
+      <div class='msg'>\
+        <p>" + msg + "</p>\
+      </div>\
+      ");
+  })
+
+  $(".textForm").on("submit", function(e){
+    e.preventDefault();
+
+    var message = $.trim($("#message").val());
+
+    socket.emit("messages", message);
+  })
+}
